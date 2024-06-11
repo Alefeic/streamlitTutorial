29c660d1bc3386d5d2623f0633024350e553d7e4
Alefeic <alessiotortoroglio+github@gmail.com>
2024-06-11 18:44:09 +0200

    prova

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 st.set_page_config(
-    page_title="La mia App",
+    page_title="La mia red:[App]",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
